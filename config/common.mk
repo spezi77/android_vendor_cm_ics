302226a9f7555c833953bd9cd84510b30bcdf192
spezi77 <spezi7713@gmail.com>
2014-05-24 13:01:04 +0200

    Added missing init script for CM [2/2]

--- config/common.mk
+++ config/common.mk
@@ -39,6 +39,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
+# CM-specific init file
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/init.local.rc:root/init.cm.rc
+
 # Swap/Zram support
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/06handleswap:system/etc/init.d/06handleswap
